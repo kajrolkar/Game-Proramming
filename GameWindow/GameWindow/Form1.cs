8c9ccc3a66720874a0c0ae4f524c95cceca7b6a2
KARAN SANJAY KAJROLKAR <karankajrolkar@gmail.com>
2021-04-28 10:03:49 +0530

    Add files via upload

---
+++ GameWindow/GameWindow/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.DirectX.Direct3D;
+
+namespace GameWindow
+{
+    public partial class Form1 : Form
+    {
+        Microsoft.DirectX.Direct3D.Device device;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+    
+        private void Render()
+        {
+            device.Clear(ClearFlags.Target, Color.Black, 0, 1);
+            device.Present();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            PresentParameters PP = new PresentParameters();
+            PP.Windowed = true;
+            PP.SwapEffect = SwapEffect.Discard;
+            device = new Device(0, DeviceType.Hardware, this, CreateFlags.HardwareVertexProcessing, PP);
+
+        }
+    }
+}
